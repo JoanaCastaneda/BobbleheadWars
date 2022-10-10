67d1d95bc1675b5a69eea21ade5f6e23ff2e0870
JoanaCastaneda <dcg.joana@hotmail.com>
2022-10-10 17:52:38 -0400

    Chapter 8 - Particles

---
+++ Assets/SelfDestruct.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    /* son para borrar cadaveres en 3 sec pero no me deja destruir los objetos pag 225
+    public float destructTime = 3.0f;
+    public void Initiate()
+    {
+        Invoke("selfDestruct", destructTime);
+    }
+    private void selfDestruct()
+    {
+        Destroy(gameObject);
+    }
+
+
+    */
+}
